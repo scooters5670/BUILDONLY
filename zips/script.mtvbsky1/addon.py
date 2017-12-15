07c24d5e32239aa135759eb7467b3b5a4ae0fc5c
biglad <biglad@mgawow.co.uk>
2017-12-15 17:02:02 +0000

    ..
    ..

--- zips/script.mtvbsky1/addon.py
+++ zips/script.mtvbsky1/addon.py
@@ -8,12 +8,14 @@ def menuoptions():
     dialog = xbmcgui.Dialog()
     funcs = (
         function1,
-        function2
+        function2,
+		function3
         )
         
     call = dialog.select('[B][COLOR=yellow]CerebroTV[/COLOR][COLOR=red] Sky 1 Links[/COLOR][/B]', [
     '[B][COLOR=white]      Sky 1 Link 1[/COLOR][/B]', 
-    '[B][COLOR=white]      Sky 1 Link 2[/COLOR][/B]'
+    '[B][COLOR=white]      Sky 1 Link 2[/COLOR][/B]',
+    '[B][COLOR=white]      Sky 1 Link 3[/COLOR][/B]'
     ])
     # dialog.selectreturns
     #   0 -> escape pressed
@@ -23,7 +25,7 @@ def menuoptions():
         # esc is not pressed
         if call < 0:
             return
-        func = funcs[call-2]
+        func = funcs[call-3]
         #dp = xbmcgui.DialogProgress()
         #dp.create("[COLOR tomato]CerebroTV[/COLOR]",""+str(func)+" -3","PLEASE EXIT KODI OR PULL THE POWER LEAD")
         #xbmc.sleep(1000)
@@ -42,5 +44,9 @@ def function2():
 
 def function1():
     xbmc.executebuiltin('PlayMedia("plugin://script.module.streamhublive/?url=swift:http://185.21.216.192:7071/routernew/SKYONE/playlist.m3u8&mode=10&name=%5BB%5D%5BCOLOR+white%5Dsky+One%5B%2FCOLOR%5D%5B%2FB%5D&iconimage=http%3A%2F%2Fswiftstreamz.com%2FSwiftStream%2Fimages%2Fthumbs%2F79832_sky11.jpg&description=sky+One+sky+One+sky+One+sky+One+sky+One+sky+One")')
-      
+
+def function3():
+    xbmc.executebuiltin('PlayMedia("plugin://plugin.video.tvone1/play_id/channels/ebcd8ef93fa99c4834d0cc3275f3dcd7")')
+
+	
 menuoptions()
